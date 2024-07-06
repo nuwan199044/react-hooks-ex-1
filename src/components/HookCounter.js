ef56c4216d109c2f6f6838c0a134492004097341
Nuwan <nuwan199044@gmail.com>
2024-07-06 22:25:05 +0530

    useEffect hook with conditional

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -1,13 +1,16 @@
 import React, {useState, useEffect} from 'react'
 
 function HookCounter() {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
 
     useEffect(() => {
+        console.log('call useEffect hook');
         document.title = `You clicked ${count} times.`
-    })
+    },[count])
   return (
     <div>
+        <input type='text' value={name} onChange={e => setName(e.target.value)}/>
         <button onClick={() => setCount(count + 1)}>Click {count} times</button>
     </div>
   )
