41c2e2d7b0729735c6b5d748fadcb76af2b20880
Nuwan <nuwan199044@gmail.com>
2024-07-06 13:47:58 +0530

    useState hook with previous state

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -1,10 +1,20 @@
 import React, {useState} from 'react'
 
 function HookCounter() {
-    const [count, setCount] = useState(0);
+    const initialState = 0;
+    const [count, setCount] = useState(initialState);
+    const increment5 = () => {
+        for(let i=0; i < 5; i++) {
+            setCount(prevCount => prevCount+1)
+        }
+    }
   return (
     <div>
-        <button onClick={() => setCount(count+1)}>Count {count}</button>
+        Count: {count}
+        <button onClick={() => setCount(initialState)}>Reset</button>
+        <button onClick={() => setCount(prevCount => prevCount+1)}>Increment</button>
+        <button onClick={() => setCount(prevCount => prevCount-1)}>Decrement</button>
+        <button onClick={increment5}>Increment 5</button>
     </div>
   )
 }
