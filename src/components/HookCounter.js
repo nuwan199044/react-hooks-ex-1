a48dd728d23d4ffb0193d9c51fdf1d7f7d9320b1
Nuwan <nuwan199044@gmail.com>
2024-07-06 21:45:36 +0530

    useState hook with array

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -1,10 +1,24 @@
 import React, {useState} from 'react'
 
 function HookCounter() {
-    const [count, setCount] = useState(0);
+    const [items, setItems] = useState([]);
+
+    const addItem = () => {
+        setItems([...items, {
+            id: items.length,
+            value: Math.floor(Math.random() * 10) + 1
+        }])
+    }
   return (
     <div>
-        <button onClick={() => setCount(count+1)}>Count {count}</button>
+        <button onClick={addItem}>Add Value</button>
+        <ul>
+            {
+                items.map(item =>(
+                    <li key={item.id}>{item.value}</li>
+                ))
+            }
+        </ul>
     </div>
   )
 }
