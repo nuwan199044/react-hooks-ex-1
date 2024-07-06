1324dceeaf9693b2a5f374cc185919f413569fff
Nuwan <nuwan199044@gmail.com>
2024-07-06 14:56:01 +0530

    useState hook with object

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -1,10 +1,13 @@
 import React, {useState} from 'react'
 
 function HookCounter() {
-    const [count, setCount] = useState(0);
+    const [name, setName] = useState({firstName: '', lastName: ''});
   return (
     <div>
-        <button onClick={() => setCount(count+1)}>Count {count}</button>
+        <input type='text' value={name.firstName} onChange={e => setName({...name, firstName: e.target.value})}/>
+        <input type='text' value={name.lastName} onChange={e => setName({...name, lastName: e.target.value})}/>
+        <h2>First Name: {name.firstName}</h2>
+        <h2>Last Name: {name.lastName}</h2>
     </div>
   )
 }
