1882e61c7cd1072641b15ef74773d406a9baa9cd
Nuwan <nuwan199044@gmail.com>
2024-07-07 19:56:48 +0530

    useEffect hook with data fetching part 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import HookCounter from './components/HookCounter';
+import DataFetching from './components/DataFetching';
 
 function App() {
   return (
     <div className="App">
-      <HookCounter />
+      <DataFetching />
     </div>
   );
 }
